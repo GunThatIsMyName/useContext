e66fbbd514c288f4acee2f17469c9b939c116b5c
Junhyun <mnmthatisus@gmail.com>
2021-08-22 18:34:42 +0900

    allrightt

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import React, { useReducer, useState } from "react";
 
-const initialState = ()=>{
-  const todos = [];
+
+const ADD="add"
+const Delete ="Delete"
+const Uncompleted="Uncompleted"
+const initState ={
+    todos:[]
 }
-const ADD = "add"
-const reducer = (state,action)=>{
-    console.log(state,"state");
-    console.log(action,"action");
-    switch (action.type){
-      case ADD:
-        return {todos:[...state.todos,{text:action.payload}]}
-      default:
-        throw Error();
+
+const reducer =(state,action)=>{
+    console.log(state,"state")
+    console.log(action,"action")
+    switch(action.type){
+        case ADD:
+          return {todos:[...state.todos,{text:action.payload,id:Math.random()}]}
+        case Delete:
+          const target = state.todos.filter(item=>item.id !== action.id)
+          return {todos:target}
+        case Uncompleted:
+          const checkTarget = state.todos.map(item=>item.id===action.id?{...item,complete:true}:item)
+          console.log(checkTarget,"맵핑")
+          return {
+            todos:checkTarget
+          }
+        default:
+          console.log("no types")
     }
 }
 
 function App() {
-  const [state,dispatch] = useReducer(reducer,initialState)
-  const [inputValue,setInputValue]=useState("")
-  const handleChange = (e)=>{
-      setInputValue(e.target.value)
-      dispatch({type:ADD})
-  }
+  const [newTodo,setTodo] = useState("")
+  const [stat,dispatch] =useReducer(reducer,initState)
   const handleSubmit =(event)=>{
       event.preventDefault();
+      dispatch({type:ADD, payload:newTodo})
+      setTodo("")
   }
+  const handleChange =(event)=>{
+      setTodo(event.target.value)
+  }
+  console.log(stat.todos,"스텟 안쪽")
   return (
     <>
-      <h1>What should i do </h1>
-      <form onSubmit={handleSubmit}>
-          <input type="text" value={inputValue} onChange={handleChange} placeholder="Write todos" />
+      <h1>TODO APP</h1>
+      <form onSubmit={handleSubmit} >
+          <input value={newTodo} onChange={handleChange} placeholder="add new todo" />
       </form>
-      <ul>
-        <p>todo list</p>
-        {state.todos.map((toDo, index) => (
-          <li key={index}>{toDo.text}</li>
-        ))}
-      </ul>
+      {stat.todos.map(item=><li style={{backgroundColor:item.complete?"orange":""}} key={item.id} >
+        <span>{item.text}</span>
+        <button onClick={()=>dispatch({type:Delete,id:item.id})} >❌</button>
+        <button onClick={()=>dispatch({type:Uncompleted,id:item.id})} >✅</button>
+        </li>)}
     </>
   );
 }
