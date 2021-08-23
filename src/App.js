41b039c1b367ae5be51ec0c3017fb812c2e8cbca
Junhyun <mnmthatisus@gmail.com>
2021-08-23 12:04:17 +0900

    all done

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import React, { useReducer, useState } from "react";
-
-
-const ADD="add"
-const Delete ="Delete"
-const Uncompleted="Uncompleted"
-const initState ={
-    todos:[]
-}
-
-const reducer =(state,action)=>{
-    console.log(state,"state")
-    console.log(action,"action")
-    switch(action.type){
-        case ADD:
-          return {todos:[...state.todos,{text:action.payload,id:Math.random()}]}
-        case Delete:
-          const target = state.todos.filter(item=>item.id !== action.id)
-          return {todos:target}
-        case Uncompleted:
-          const checkTarget = state.todos.map(item=>item.id===action.id?{...item,complete:true}:item)
-          console.log(checkTarget,"맵핑")
-          return {
-            todos:checkTarget
-          }
-        default:
-          console.log("no types")
-    }
-}
+import { Delete, Uncompleted } from "./action";
+import Add from "./Add";
+import { useDispatch, useTodos } from "./Context";
+import List from "./list";
+import Todo from "./todo";
 
 function App() {
-  const [newTodo,setTodo] = useState("")
-  const [stat,dispatch] =useReducer(reducer,initState)
-  const handleSubmit =(event)=>{
-      event.preventDefault();
-      dispatch({type:ADD, payload:newTodo})
-      setTodo("")
-  }
-  const handleChange =(event)=>{
-      setTodo(event.target.value)
-  }
-  console.log(stat.todos,"스텟 안쪽")
+  const todos =useTodos();
   return (
     <>
       <h1>TODO APP</h1>
-      <form onSubmit={handleSubmit} >
-          <input value={newTodo} onChange={handleChange} placeholder="add new todo" />
-      </form>
-      {stat.todos.map(item=><li style={{backgroundColor:item.complete?"orange":""}} key={item.id} >
-        <span>{item.text}</span>
-        <button onClick={()=>dispatch({type:Delete,id:item.id})} >❌</button>
-        <button onClick={()=>dispatch({type:Uncompleted,id:item.id})} >✅</button>
-        </li>)}
+      <Add />
+      <List name={"TO DO"} >
+      {todos.map(item=>(<Todo key={item.id}  complete={item.complete} id={item.id} text={item.text} />))}
+      </List>
     </>
   );
 }
